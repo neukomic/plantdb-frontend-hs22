141caf32471c1167a9e666a4199264d517cd1224
neukomic <neukomic@students.zhaw.ch>
2023-01-19 00:02:08 +0100

    Made it nice

--- src/pages/AboutUs.svelte
+++ src/pages/AboutUs.svelte
@@ -1,8 +1,29 @@
 <h1>About us</h1>
 <hr class="divider" />
 
-<p>See which cars are still available and pick your favorite!</p>
-<p>Check who owns the car and contact them directly, no need for an account!</p>
+<p>
+    Welcome to our startup! We are a team of passionate and dedicated
+    individuals who have come together to create something truly special. Our
+    goal is to revolutionize the industry with our innovative products and
+    services.
+</p>
+<p>
+    We are a company that is driven by a desire to make a difference in the
+    world. We strive to create solutions that will improve the lives of our
+    customers and make the world a better place. We believe that by staying true
+    to our values and being dedicated to our craft, we can achieve great things.
+</p>
+<p>
+    Our team is made up of experts in their respective fields, from software
+    development to product design. We are constantly working to push the
+    boundaries of what is possible and create products that are truly
+    cutting-edge.
+</p>
+<p>
+    We are excited to share our vision with the world and can't wait for you to
+    see what we have in store. Thank you for taking the time to learn about us
+    and we hope to have the opportunity to work with you in the near future.
+</p>
 
 <div class="container">
     <div class="col-md-12 text-center">
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,14 +1,86 @@
-<h1>Welcome to Zoom Car Rentals</h1>
-<hr class="divider" />
-
-<p>See which cars are still available and pick your favorite!</p>
-<p>Check who owns the car and contact them directly, no need for an account!</p>
-
-<div class="container">
-    <div class="col-md-12 text-center">
-        <a class="btn btn-primary" href="#/cars">Go to available cars</a>
+<!-- Header-->
+<header>
+    <div class="container px-5">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-lg-6">
+                <div class="text-center my-5">
+                    <h1 class="display-5 fw-bolder mb-2">
+                        Welcome to Zoom Car Rentals
+                    </h1>
+                    <p />
+                    <p class="lead text-black-25 mb-4">
+                        See which cars are still available and pick your
+                        favorite! Check who owns the car and contact them
+                        directly, no need for an account!
+                    </p>
+                    <div
+                        class="d-grid gap-3 d-sm-flex justify-content-sm-center"
+                    >
+                        <a
+                            class="btn btn-primary btn-lg px-4 me-sm-3"
+                            href="#/cars">Go to available cars</a
+                        >
+                        <a
+                            class="btn btn-outline-dark btn-lg px-4 text-black"
+                            href="#/about-us">About us</a
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+</header>
+
+<!-- Features section-->
+<section id="features">
+    <div class="container px-5 my-5">
+        <div class="text-center row gx-5">
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <div
+                    class="feature bg-primary bg-gradient text-white rounded-3 mb-3"
+                />
+                <h2 class="h4 fw-bolder">Convenience</h2>
+                <p>
+                    Welcome to our innovative car rental startup! We are a team
+                    of car enthusiasts and industry experts who have come
+                    together to revolutionize the way you rent cars. Our goal is
+                    to make car rental more convenient, affordable and
+                    eco-friendly. We offer a wide range of vehicles from luxury
+                    cars to electric vehicles, all at competitive prices.
+                </p>
+            </div>
+            <div class="col-lg-4 mb-5 mb-lg-0">
+                <div
+                    class="feature bg-primary bg-gradient text-white rounded-3 mb-3"
+                />
+                <h2 class="h4 fw-bolder">Sustainability</h2>
+                <p>
+                    Our platform is user-friendly and easy to use, you can
+                    browse and book your car from the comfort of your own home.
+                    We offer flexible rental options, whether you need a car for
+                    a day, a week or a month, we have you covered. We also have
+                    a strong focus on sustainability, our fleet includes a
+                    variety of electric vehicles, and we are constantly working
+                    to reduce our carbon footprint.
+                </p>
+            </div>
+            <div class="col-lg-4">
+                <div
+                    class="feature bg-primary bg-gradient text-white rounded-3 mb-3"
+                />
+                <h2 class="h4 fw-bolder">Flexibility</h2>
+                <p>
+                    We pride ourselves on providing excellent customer service,
+                    and our team is always on hand to answer any questions you
+                    may have. Thank you for choosing us, we can't wait to get
+                    you on the road! Paragraph of text beneath the heading to
+                    explain the heading. We'll add onto it with another sentence
+                    and probably just keep going until we run out of words.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
 <style>
     h1 {
